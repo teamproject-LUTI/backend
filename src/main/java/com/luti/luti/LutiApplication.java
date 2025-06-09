1c07692b569397c296f2f7de35083f183bf78882
Lifesheep1 <165856601+Lifesheep1@users.noreply.github.com>
2025-06-09 13:34:32 +0900

    Add files via upload

---
+++ src/main/java/com/luti/luti/LutiApplication.java
@@ -0,0 +1,13 @@
+package com.luti.luti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LutiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luti/luti/LutiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luti.luti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LutiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
