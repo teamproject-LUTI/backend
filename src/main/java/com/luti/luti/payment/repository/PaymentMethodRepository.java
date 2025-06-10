84908daa1343d8cc3baab968e09f62e6c6459983
김원기 <hina361@naver.com>
2025-06-10 14:20:37 +0900

    Revert "결제 엔티티 추가"
    This reverts commit 2ea51df623a65fad1bd4a35e1ccf6e7300cae879.

--- src/main/java/com/luti/luti/payment/repository/PaymentMethodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.luti.luti.payment.repository;
-
-import com.luti.luti.payment.entity.PaymentMethod;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentMethodRepository extends JpaRepository<PaymentMethod, Integer> {
-
-}
