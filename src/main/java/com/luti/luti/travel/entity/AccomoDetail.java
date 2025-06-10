26fdba15c251f035253cb2b79c8146117582d49c
01MUN <kang66ta@gmail.com>
2025-06-10 14:45:22 +0900

    feat: #11 travel 엔티티 설계

---
+++ src/main/java/com/luti/luti/travel/entity/AccomoDetail.java
@@ -0,0 +1,55 @@
+package com.luti.luti.travel.entity;
+
+import com.luti.luti.auth.entity.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "accomodetail")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AccomoDetail {
+
+    @Id
+    @Column(name = "payment_ownno")
+    private Long paymentOwnno;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_no")
+    public PaymentList paymentNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "accomo_no")
+    public AccomoInfo accomoNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "login_id")
+    public User loginId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "payment_cd")
+    public PaymentList paymentCd;
+
+    @Column(name = "price")
+    private Long price;
+
+    @Column(name = "accomo_start")
+    private Date accomoStart;
+
+    @Column(name = "accomo_end")
+    private Date accomoEnd;
+
+    @Column(name = "user_count")
+    private Long userCount;
+
+    @Column(name = "room_type")
+    private String roomType;
+
+
+}
