40b44e4abfd66a76086616f8494ff835a878048d
김원기 <hina361@naver.com>
2025-06-10 15:09:45 +0900

    FEAT : #10 payment 엔티티 추가

---
+++ src/main/java/com/luti/luti/payment/antity/PaymentMethod.java
@@ -0,0 +1,26 @@
+package com.luti.luti.payment.antity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "paymentmethod")
+public class PaymentMethod {
+
+    @Id
+    @Column(name = "payment_cd")
+    private Integer paymentCd; // 결제방식코드 (예: 1=카드, 2=현금 등)
+
+    @Column(name = "payment_method", length = 50)
+    private String paymentMethod; // 결제방식 이름 (예: "신용카드", "현금")
+
+    @OneToMany(mappedBy = "paymentMethod")
+    private List<PaymentList> paymentLists = new ArrayList<>();  // 이 결제방식을 참조하는 모든 결제내역들
+
+}
