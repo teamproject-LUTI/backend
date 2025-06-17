dc17ae5ea57ab773e6cb4c613233dd6a5e5181b3
김원기 <hina361@naver.com>
2025-06-17 11:24:48 +0900

    ㅇ

--- src/main/java/com/luti/payment/entity/PaymentList.java
+++ src/main/java/com/luti/payment/entity/PaymentList.java
@@ -14,6 +14,7 @@ import java.time.LocalDate;
 @Table(name = "payment_list")
 public class PaymentList {
 
+    ///  아아앙아ㅏㅇ아
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "payment_id", nullable = false)
