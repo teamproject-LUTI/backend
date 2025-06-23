80a83287d53337c8548a4526027598a77444a8e4
subin kim <subinkim_96@naver.com>
2025-06-23 10:47:34 +0900

    FEAT : 검색기능 추가

--- src/main/java/com/luti/board/repository/ReviewRepository.java
+++ src/main/java/com/luti/board/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.luti.board.repository;
 
 import com.luti.board.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+public interface ReviewRepository extends JpaRepository<Review, Long>, JpaSpecificationExecutor<Review> {
 
     /** soft-delete=false 인 후기만 조회 */
     List<Review> findAllByIsDeletedFalse();
